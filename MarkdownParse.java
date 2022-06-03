a736c98fafd47c0c997b68082c9d3d595d972887
Jenny Quach <65835856+hoatuyet423@users.noreply.github.com>
2022-06-03 12:49:02 -0700

    Updated CommonMark

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -2,12 +2,10 @@
 // change something 
 //some changes
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class MarkdownParse {
 
